6765c79e633608098735631d834866e181ccfc8c
Matthew Palmacci <mpalmacci@gmail.com>
2012-12-18 00:09:24 -0500

    Unsure what happened really
    I've been trying to move the location of the codebase on the Mac.  I
    had a lot of issues with it.  Checking in because both apps seem to be
    running.

--- gen/com/magichat/R.java
+++ gen/com/magichat/R.java
@@ -154,8 +154,7 @@ public final class R {
         public static final int play_game_menu=0x7f0b0002;
     }
     public static final class raw {
-        public static final int cards=0x7f050000;
-        public static final int deck_list=0x7f050001;
+        public static final int deck_list=0x7f050000;
     }
     public static final class string {
         public static final int active_deck=0x7f080018;
